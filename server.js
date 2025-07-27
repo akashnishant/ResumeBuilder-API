c41453b449ecd8d856893a7300dd9f127ed47e63
Akash Nishant <akash.nishant@itechnolabs.tech>
2025-07-27 15:52:26 +0530

    Added dockerfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const puppeteer = require('puppeteer');
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' })); // Handle large HTML
@@ -39,6 +39,6 @@ app.post('/download-pdf', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
